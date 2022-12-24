561b36d87d00c0e57d5940eb50a3b24b8709abf2
Atul <shinedeatulramesh@gmail.com>
2022-12-24 19:38:20 +0530

    First commit : code

---
+++ src/com/demo/ArrayListDemo.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> list=new ArrayList<String>();
+		list.add("Atul");
+		list.add("Abhi");
+		list.add("kartik");
+		
+		System.out.println(list);
+	}
+}
